ad6d124d0e2b1c0c056cb562f7e70a183b134946
standers <standers@nerdery.com>
2014-09-11 00:05:47 -0500

    adding root yeoman app.

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name troutApp
+ * @description
+ * # troutApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('troutApp', [
+    'ngAnimate',
+    'leaflet-directive'
+  ]);
